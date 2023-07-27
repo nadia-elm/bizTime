9488b10b3f54a69fc7ed2a97b9b696b854b6a5a5
Your Name <you@example.com>
2023-07-27 11:18:25 +0400

    allow paying for invoices / update put/invoices/id

--- routes/companies.js
+++ routes/companies.js
@@ -1,5 +1,6 @@
 
 const express = require("express");
+const slugify = require('slugify')
 const router = express.Router();
 const db = require("../db");
 const ExpressError = require("../expressError");
@@ -39,7 +40,8 @@ router.get("/:code", async (req, res, next) => {
 // create new company
 router.post("/", async (req, res, next) => {
   try {
-    const { code, name, description } = req.body;
+    const { name, description } = req.body;
+    const code = slugify(name)
     const result = await db.query(
       `INSERT INTO companies (code, name, description)
              VALUES ($1, $2, $3)
--- routes/invoices.js
+++ routes/invoices.js
@@ -7,7 +7,7 @@ const ExpressError = require('../expressError')
 router.get("/", async (req, res, next) => {
   try {
     const result = await db.query(
-      `SELECT id, comp_code, amt, paid, add_date, paid_date FROM invoices`
+      `SELECT id, comp_code, amt, paid, add_date, paid_date , paying FROM invoices`
     );
     return res.json({ invoices: result.rows });
   } catch (e) {
@@ -47,21 +47,98 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+// router.put("/:id", async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const { amt, paying} = req.body
+
+//     const invoiceResult = await db.query(
+//         `SELECT paid, paid_date FROM invoices WHERE id = $1`,[1]
+//     );
+
+//     if(invoiceResult.rows.length === 0){
+//         throw new ExpressError('Invoice not found', 404)
+//     }
+
+//     const {paid, paid_date} = invoiceResult.rows[0]
+//     if(paying === true && !paid) {
+//         const result = await db.query (`
+//         UPDATE invoices SET amt = $1, paid = true,
+//          paid_date = CURRENT_DATE 
+//          WHERE id = $2
+//          RETURNING id, amt, paid, paid_date`
+//          [amt, id]
+//          )
+//          if(result.rows.length === 0){
+//          throw new ExpressError("Update failed", 500);   
+//          }
+//          return res.json({ invoice : result.row[0]})
+//     }else{
+//         // if not paying or already paid update only amt
+//         const result = await db.query(
+//             `UPDATE invoices SET amt = $1 WHERE id = $2 
+//             RETURNING id, amt, paid, paid_date`,
+//             [amt,id]
+//         )
+//         if (result.rows.length === 0) {
+//           throw new ExpressError("Update failed", 500);
+//         }
+
+//         return res.json({ invoice: result.rows[0] });
+//     }
+    
+    
+//   } catch (err) {
+//     return next(err);
+//   }
+// });
+
+
+
 router.put("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const result = await db.query(
-      `UPDATE invoices SET  amt = $1 WHERE id =$2 RETURNING id,amt`,
-      [req.body.amt, id]
+    const { amt, paying } = req.body;
+
+    const invoiceResult = await db.query(
+      `SELECT paid, paid_date FROM invoices WHERE id = $1`,
+      [id]
     );
-    if (result.rows.length === 0) {
-      throw new ExpressError("invoices not found", 404);
+
+    if (invoiceResult.rows.length === 0) {
+      throw new ExpressError("Invoice not found", 404);
     }
-    return res.json({ invoice: result.rows[0] });
+
+    const { paid, paid_date } = invoiceResult.rows[0];
+    if (paying === true && !paid) {
+      const result = await db.query(
+        `UPDATE invoices SET amt = $1, paid = true, paid_date = CURRENT_DATE WHERE id = $2 RETURNING id, amt, paid, paid_date`,
+        [amt, id]
+      );
+
+      if (result.rows.length === 0) {
+        throw new ExpressError("Update failed", 500);
+      }
+
+      return res.json({ invoice: result.rows[0] });
+    } else {
+      // if not paying or already paid, update only the amt field
+      const result = await db.query(
+        `UPDATE invoices SET amt = $1 WHERE id = $2 RETURNING id, amt, paid, paid_date`,
+        [amt, id]
+      );
+
+      if (result.rows.length === 0) {
+        throw new ExpressError("Update failed", 500);
+      }
+
+      return res.json({ invoice: result.rows[0] });
+    }
   } catch (err) {
     return next(err);
   }
 });
+
 
 router.delete("/:id", async (req, res, next) => {
   try {
